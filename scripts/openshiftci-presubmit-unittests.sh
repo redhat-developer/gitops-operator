abe96ba18c066796194a9d5f6549378d5af5dbb3
Dewan Ishtiaque Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2020-11-26 08:33:37 -0500

    Adding scripts for all-tests and unit-tests (#18)

---
+++ scripts/openshiftci-presubmit-unittests.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# fail if some commands fails
+set -e
+# show commands
+set -x
+
+export ARTIFACTS_DIR="/tmp/artifacts"
+export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
+export PATH=$PATH:$GOPATH/bin
+
+go env
+if [[ $(go fmt `go list ./... | grep -v vendor`) ]]; then
+    echo "not well formatted sources are found"
+    exit 1
+fi
+go mod tidy
+if [[ ! -z $(git status -s) ]]
+then
+    echo "Go mod state is not clean."
+    exit 1
+fi
+echo "Add your unit test target"
+
