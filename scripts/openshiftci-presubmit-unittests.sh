de4cbd3110e84ec3f9c6ef25a2ebe3f326deeff2
arout <arout@redhat.com>
2021-02-17 11:29:04 +0530

    Adding vendor path

--- scripts/openshiftci-presubmit-unittests.sh
+++ scripts/openshiftci-presubmit-unittests.sh
@@ -10,6 +10,7 @@ export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
 export PATH=$PATH:$GOPATH/bin
 
 go env
+go mod vendor
 if [[ $(go fmt `go list ./... | grep -v vendor`) ]]; then
     echo "not well formatted sources are found"
     exit 1
