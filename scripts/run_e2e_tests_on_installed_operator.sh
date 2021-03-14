a4a70c4368dfdfead836df9b1fcdcae94931222c
Shoubhik Bose <shbose@redhat.com>
2021-03-13 23:46:06 -0500

    set no-setup

--- scripts/run_e2e_tests_on_installed_operator.sh
+++ scripts/run_e2e_tests_on_installed_operator.sh
@@ -2,16 +2,11 @@
 
 set -x
 
-E2E_TEST_NS="gitops-test"
 E2E_TEST_DIR=./test/e2e
 ARGOCD_NS="openshift-gitops"
 DEPRACATED_ARGOCD_NS="openshift-pipelines-app-delivery"
 CONSOLE_LINK="argocd"
 
-# Create a new namespace for e2e tests
-#oc new-project $E2E_TEST_NS
-
-# Point to the internal API server hostname
 APISERVER=https://kubernetes.default.svc
 
 # Path to ServiceAccount token
@@ -23,10 +18,6 @@ NAMESPACE=$(cat ${SERVICEACCOUNT}/namespace)
 # Read the ServiceAccount bearer token
 TOKEN=$(cat ${SERVICEACCOUNT}/token)
 
-# Reference the internal certificate authority (CA)
-CACERT=$(cat ${SERVICEACCOUNT}/ca.crt)
-
-APISERVER=https://kubernetes.default.svc
 
 echo "
 apiVersion: v1
@@ -34,17 +25,17 @@ kind: Config
 clusters:
 - name: default-cluster
   cluster:
-    certificate-authority-data: ${CACERT}
+    insecure-skip-tls-verify: true
     server: ${APISERVER}
 contexts:
 - name: default-context
   context:
     cluster: default-cluster
     namespace: ${NAMESPACE}
-    user: default-user
+    user: admin
 current-context: default-context
 users:
-- name: default-user
+- name: admin
   user:
     token: ${TOKEN}
 " > sa.kubeconfig
@@ -52,7 +43,4 @@ users:
 
 
 echo "Running e2e tests"
-SKIP_OPERATOR_DEPLOYMENT=true operator-sdk test local $E2E_TEST_DIR --operator-namespace default --kubeconfig=/tmp/gitops-operator/sa.kubeconfig --verbose 
-
-echo "Cleaning e2e test resources"
-#oc delete project $E2E_TEST_NS
+CGO_ENABLED=0 SKIP_OPERATOR_DEPLOYMENT=true operator-sdk test local $E2E_TEST_DIR  --kubeconfig=/tmp/gitops-operator/sa.kubeconfig --verbose --no-setup
