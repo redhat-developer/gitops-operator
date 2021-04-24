697a419b67b30cc1fcb81bd0db716ce17c8262f6
Abhishek Veeramalla <abhishek.veeramalla@gmail.com>
2021-04-24 09:51:39 +0530

    feat: set default requests and limits (#92) (#108)
    * feat: set default requests and limits (#92)
    
    Signed-off-by: iam-veeramalla <abhishek.veeramalla@gmail.com>
    
    * Update pkg/controller/argocd/argocd.go
    
    * Update pkg/controller/argocd/argocd.go
    
    Co-authored-by: William Tam <wtam@redhat.com>

---
+++ pkg/controller/argocd/argocd_test.go
@@ -0,0 +1,109 @@
+package argocd
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+	v1 "k8s.io/api/core/v1"
+	resourcev1 "k8s.io/apimachinery/pkg/api/resource"
+)
+
+func TestArgoCD(t *testing.T) {
+	testArgoCD, _ := NewCR("openshift-gitops", "openshift-gitops")
+
+	testApplicationSetResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("512Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("1000m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("1024Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("2000m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.ApplicationSet.Resources, testApplicationSetResources)
+
+	testControllerResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("1024Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("1000m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("2048Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("2000m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Controller.Resources, testControllerResources)
+
+	testDexResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("128Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("250m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Dex.Resources, testDexResources)
+
+	testGrafanaResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("128Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("250m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Grafana.Resources, testGrafanaResources)
+
+	testHAProxyResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("128Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("250m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.HA.Resources, testHAProxyResources)
+
+	testRedisResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("128Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("250m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Redis.Resources, testRedisResources)
+
+	testRepoResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("512Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("1000m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Repo.Resources, testRepoResources)
+
+	testServerResources := &v1.ResourceRequirements{
+		Requests: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("128Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("125m"),
+		},
+		Limits: v1.ResourceList{
+			v1.ResourceMemory: resourcev1.MustParse("256Mi"),
+			v1.ResourceCPU:    resourcev1.MustParse("500m"),
+		},
+	}
+	assert.DeepEqual(t, testArgoCD.Spec.Server.Resources, testServerResources)
+}
