70d69e25ee20740531d4e7986d94f872d6b2acc6
arout <arout@localhost.localdomain>
2021-06-15 15:30:32 +0530

    Checking application sync and health status

---
+++ test/e2e/helper/application_status.go
@@ -0,0 +1,50 @@
+package helper
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func ApplicationHealthStatus(appname string, namespace string) error {
+	var stdout bytes.Buffer
+	ocPath, err := exec.LookPath("oc")
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(ocPath, "get", "application", "-n", namespace, "-o", "jsonpath='{.items[?(@.metadata.name==\""+appname+"\")].status.health.status}'")
+	err = cmd.Run()
+	cmd.Stdout = &stdout
+	if err != nil {
+		return err
+	}
+
+	if strings.Trim(stdout.String(), " ") != "Healthy" {
+		fmt.Errorf(stdout.String())
+	}
+
+	return nil
+}
+
+func ApplicationSyncStatus(appname string, namespace string) error {
+	var stdout bytes.Buffer
+	ocPath, err := exec.LookPath("oc")
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(ocPath, "get", "application", "-n", namespace, "-o", "jsonpath='{.items[?(@.metame==\""+appname+"\")].status.sync.status}'")
+	err = cmd.Run()
+	cmd.Stdout = &stdout
+	if err != nil {
+		return err
+	}
+
+	if strings.Trim(stdout.String(), " ") != "Synced" {
+		fmt.Errorf(stdout.String())
+	}
+
+	return nil
+}
