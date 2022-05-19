5d3ee6d34990aabcbbf4ce3c0ff7df3f908dcc48
rishabh625 <rishabhmishra625@gmail.com>
2022-05-19 17:14:00 +0530

    Added echo
    Signed-off-by: rishabh625 <rishabhmishra625@gmail.com>

--- scripts/openshiftci-presubmit-unittests.sh
+++ scripts/openshiftci-presubmit-unittests.sh
@@ -9,6 +9,12 @@ export PATH=$PATH:$GOPATH/bin
 
 go env
 go mod vendor
+op=$(find / -name controller-gen)
+echo $op
+op1=$(ls /go/bin/)
+op2=$(shell ls bin/)
+echo $op1
+echo $op2
 if [[ $(go fmt `go list ./... | grep -v vendor`) ]]; then
     echo "not well formatted sources are found"
     exit 1
