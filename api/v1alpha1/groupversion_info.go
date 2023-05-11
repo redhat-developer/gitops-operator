122992faa500c857d143729cad3fa48684c05e72
Yi Cai <yicai@redhat.com>
2023-05-11 15:24:15 -0400

    format fix

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the pipelines v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=pipelines.openshift.io
+// +kubebuilder:object:generate=true
+// +groupName=pipelines.openshift.io
 package v1alpha1
 
 import (
