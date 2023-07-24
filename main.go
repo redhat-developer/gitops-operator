ad3b52a803abf04ea4110e0c05a35cbdef861b8c
Yi Cai <yicai@redhat.com>
2023-07-24 09:54:54 -0400

    Fixed log timestamp format (#574)
    Co-authored-by: Jaideep Rao <jaideep.r97@gmail.com>

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+	"go.uber.org/zap/zapcore"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -83,6 +84,7 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
 		Development: true,
+		TimeEncoder: zapcore.RFC3339TimeEncoder,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
