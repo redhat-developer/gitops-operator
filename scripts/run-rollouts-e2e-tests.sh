75666e7188c7b9adc1db6c92542044f8b7bf4040
Jonathan West <jgwest@users.noreply.github.com>
2024-06-06 09:33:38 -0400

    Add script to run Rollouts tests against gitops-operator repo (#723)
    Signed-off-by: Jonathan West <jonwest@redhat.com>
    Signed-off-by: Anand Francis Joseph <anjoseph@redhat.com>

---
+++ scripts/run-rollouts-e2e-tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# The goal of this script is to run the Argo Rollouts operator tests from the argo-rollouts-manager repo against gitops-operator:
+# - Runs the (cluster-scoped) E2E tests of the Argo Rollouts operator
+# - Runs the upstream E2E tests from the argo-rollouts repo
+
+ROLLOUTS_TMP_DIR=$(mktemp -d)
+
+cd $ROLLOUTS_TMP_DIR
+
+git clone https://github.com/argoproj-labs/argo-rollouts-manager
+
+cd "$ROLLOUTS_TMP_DIR/argo-rollouts-manager"
+
+# This commit value will be automatically updated by calling 'hack/upgrade-rollouts-manager/go-run.sh':
+# - It should always point to the same argo-rollouts-manager commit that is referenced in go.mod of gitops-operator (which will usually be the most recent argo-rollouts-manager commit)
+TARGET_ROLLOUT_MANAGER_COMMIT=b3e573f0e6ea3a5ec3eeba70ebb5d90e58efcd68
+
+git checkout $TARGET_ROLLOUT_MANAGER_COMMIT
+
+# 1) Run E2E tests from argo-rollouts-manager repo
+
+make test-e2e
+
+# Clean up old namespaces created by test
+# NOTE: remove this once this is handled by 'make test-e2e' in argo-rollouts-manager repo
+kubectl delete rolloutmanagers --all -n test-rom-ns-1
+
+cd "$ROLLOUTS_TMP_DIR/argo-rollouts-manager"
+
+
+# 2) Run E2E tests from argoproj/argo-rollouts repo
+
+SKIP_RUN_STEP=true hack/run-upstream-argo-rollouts-e2e-tests.sh
