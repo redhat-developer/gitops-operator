af59bb5caf9da560a8b40807517b252d6994d21b
Siddhesh Ghadi <sghadi1203@gmail.com>
2025-07-30 18:45:07 +0530

    Add config ingress permission
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- controllers/gitopsservice_controller.go
+++ controllers/gitopsservice_controller.go
@@ -199,6 +199,7 @@ type ReconcileGitopsService struct {
 //+kubebuilder:rbac:groups=argoproj.io,resources=notificationsconfigurations;notificationsconfigurations/finalizers,verbs=*
 //+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=get;list;watch;
 //+kubebuilder:rbac:groups="apiregistration.k8s.io",resources="apiservices",verbs=get;list
+// +kubebuilder:rbac:groups=config.openshift.io,resources=ingresses,verbs=get;list;watch
 
 // Reconcile reads that state of the cluster for a GitopsService object and makes changes based on the state read
 // and what is in the GitopsService.Spec
