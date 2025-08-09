6cc6c4cc23cf4cc615a2ce2a9999def7ab7d47ee
Jonathan West <jonwest@redhat.com>
2025-08-09 09:20:09 -0400

    Update

--- scripts/openshift-CI-kuttl-tests.sh
+++ scripts/openshift-CI-kuttl-tests.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+echo WTF1
+
 export CI="prow"
 go mod vendor
 
@@ -10,6 +12,7 @@ source $(dirname $0)/e2e-common.sh
 # Script entry point.
 KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
 
+echo WTF2
 # Copy kubeconfig to temporary kubeconfig file and grant read and Write permission to temporary kubeconfig file
 TMP_DIR=$(mktemp -d)
 cp $KUBECONFIG $TMP_DIR/kubeconfig
@@ -17,10 +20,13 @@ chmod 640 $TMP_DIR/kubeconfig
 export KUBECONFIG=$TMP_DIR/kubeconfig
 cp $KUBECONFIG /go/src/github.com/redhat-developer/gitops-operator/kubeconfig
 
+echo WTF3
 # Ensuring proper installation
 pod=openshift-gitops-operator-controller-manager && oc get pods `oc get pods --all-namespaces | grep $pod | head -1 | awk '{print $2}'` -n openshift-gitops-operator -o yaml
 subscription=gitops-operator- && oc get subscription `oc get subscription --all-namespaces | grep $subscription | head -1 | awk '{print $2}'` -n openshift-gitops-operator
 oc wait --for=condition=Ready -n openshift-gitops pod --timeout=15m  -l 'app.kubernetes.io/name in (cluster,openshift-gitops-application-controller,openshift-gitops-applicationset-controller,openshift-gitops-dex-server,openshift-gitops-redis,openshift-gitops-repo-server,openshift-gitops-server)' 
+
+echo WTF4
 
 # Check argocd instance creation
 oc create ns test-argocd
--- scripts/openshift-interop-kuttl-tests.sh
+++ scripts/openshift-interop-kuttl-tests.sh
@@ -4,11 +4,15 @@ set -ex
 
 go mod vendor
 
+
+
 source $(dirname $0)/e2e-common.sh
 
+echo AWTF1
 # Script entry point.
 KUBECONFIG=${KUBECONFIG:-$HOME/.kube/config}
 
+echo AWTF2
 # Copy kubeconfig to temporary kubeconfig file and grant read and Write permission to temporary kubeconfig file
 TMP_DIR=$(mktemp -d)
 cp $KUBECONFIG $TMP_DIR/kubeconfig
@@ -16,10 +20,13 @@ chmod 640 $TMP_DIR/kubeconfig
 export KUBECONFIG=$TMP_DIR/kubeconfig
 cp $KUBECONFIG /go/src/github.com/redhat-developer/gitops-operator/kubeconfig
 
+echo AWTF3
 # Ensuring proper installation
 pod=openshift-gitops-operator-controller-manager && oc get pods `oc get pods --all-namespaces | grep $pod | head -1 | awk '{print $2}'` -n openshift-gitops-operator -o yaml
 oc get subscription -n openshift-gitops-operator
 oc wait --for=condition=Ready -n openshift-gitops pod --timeout=15m  -l 'app.kubernetes.io/name in (cluster,openshift-gitops-application-controller,openshift-gitops-applicationset-controller,openshift-gitops-dex-server,openshift-gitops-redis,openshift-gitops-repo-server,openshift-gitops-server)' 
+
+echo AWTF3
 
 # Check argocd instance creation
 oc create ns test-argocd
