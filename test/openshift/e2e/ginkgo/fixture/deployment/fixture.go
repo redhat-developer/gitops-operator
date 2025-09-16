6c230e278199cc25fa2c859b6a9f36d259fcb1c6
Anand Francis Joseph <anjoseph@redhat.com>
2025-09-16 12:25:56 +0530

    Added initial version of e2e test which needs improvment and additional tests
    Signed-off-by: Anand Francis Joseph <anjoseph@redhat.com>

--- test/openshift/e2e/ginkgo/fixture/deployment/fixture.go
+++ test/openshift/e2e/ginkgo/fixture/deployment/fixture.go
@@ -364,6 +364,14 @@ func HaveServiceAccountName(expectedServiceAccountName string) matcher.GomegaMat
 	})
 }
 
+// HaveResourceRequirements validates if the deployment object contains the given resource requirements.
+func HaveResourceRequirements(requirements *corev1.ResourceRequirements) matcher.GomegaMatcher {
+	return fetchDeployment(func(depl *appsv1.Deployment) bool {
+		GinkgoWriter.Println("Deployment HaveResourceRequirements:", "expected: ", requirements.String(), "actual: ", depl.Spec.Template.Spec.Resources.String())
+		return reflect.DeepEqual(requirements, depl.Spec.Template.Spec.Resources)
+	})
+}
+
 // This is intentionally NOT exported, for now. Create another function in this file/package that calls this function, and export that.
 func fetchDeployment(f func(*appsv1.Deployment) bool) matcher.GomegaMatcher {
 
