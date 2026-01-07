261cc3d6e1733c95f7eb66c9db18ba3ff2dc0c92
Siddhesh Ghadi <61187612+svghadi@users.noreply.github.com>
2026-01-07 22:56:05 +0530

    chore: Update argocd-operator and k8s deps (#1042)
    * Update argocd-operator and k8s deps
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * Use go 1.25
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * pin go to 1.25.3 in ci
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * Generate manifests
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * Revert controller-gen.kubebuilder.io/version annotation
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * Update argocd version in toolchain test
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    * Update redis version in test
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>
    
    ---------
    
    Signed-off-by: Siddhesh Ghadi <sghadi1203@gmail.com>

--- test/openshift/e2e/ginkgo/parallel/1-031_validate_toolchain_test.go
+++ test/openshift/e2e/ginkgo/parallel/1-031_validate_toolchain_test.go
@@ -87,7 +87,7 @@ var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 			// These variables need to be maintained according to the component matrix: https://spaces.redhat.com/display/GITOPS/GitOps+Component+Matrix
 			expected_kustomizeVersion := "v5.7.0"
 			expected_helmVersion := "v3.18.4"
-			expected_argocdVersion := "v3.1.9"
+			expected_argocdVersion := "v3.2.1"
 
 			var expected_dexVersion string
 			var expected_redisVersion string
@@ -95,7 +95,7 @@ var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 			if os.Getenv("CI") == "prow" {
 				// when running against openshift-ci
 				expected_dexVersion = "v2.43.0"
-				expected_redisVersion = "7.2.11"
+				expected_redisVersion = "8.2.2"
 
 			} else {
 				// when running against RC/ released version of gitops
