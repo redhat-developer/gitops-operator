ecf92512b05793e104d583aa3073127ac73ab2ad
akhil nittala <nakhil@redhat.com>
2026-02-23 15:16:39 +0530

    fix: restoring tracking labels for cleaning up of orphaned roles and rolebindings
    Signed-off-by: akhil nittala <nakhil@redhat.com>

--- test/openshift/e2e/ginkgo/parallel/1-031_validate_toolchain_test.go
+++ test/openshift/e2e/ginkgo/parallel/1-031_validate_toolchain_test.go
@@ -85,7 +85,7 @@ var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 		It("verifies that toolchain versions have the expected values", func() {
 
 			// These variables need to be maintained according to the component matrix: https://spaces.redhat.com/display/GITOPS/GitOps+Component+Matrix
-			expected_kustomizeVersion := "v5.7.0"
+			expected_kustomizeVersion := "v5.8.0"
 			expected_helmVersion := "v3.18.4"
 			expected_argocdVersion := "v3.2.1"
 
