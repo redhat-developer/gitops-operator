fa6f4abbccc9bdc4aa43f6aad0643a94b4433f54
Varsha B <vab@redhat.com>
2026-02-27 22:50:02 +0530

    Modify tests for interop test suite (#1076)
    * modify tests for interop
    
    Signed-off-by: Varsha B <vab@redhat.com>
    
    * move the test to sequential
    
    Signed-off-by: Varsha B <vab@redhat.com>
    
    ---------
    
    Signed-off-by: Varsha B <vab@redhat.com>

--- test/openshift/e2e/ginkgo/sequential/1-077_validate_disable_dex_removed_test.go
+++ test/openshift/e2e/ginkgo/sequential/1-077_validate_disable_dex_removed_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package parallel
+package sequential
 
 import (
 	"context"
@@ -30,7 +30,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
+var _ = Describe("GitOps Operator Sequential E2E Tests", func() {
 
 	Context("1-077_validate_disable_dex_removed", func() {
 
