18bff867e1bed6a1a351ae8fba8bd26a806d4dc1
nmirasch <neus.miras@gmail.com>
2026-03-26 18:40:53 +0100

    assert podSecurityLabelSync and audit=restricted for openshift-gitops ns
    Signed-off-by: nmirasch <neus.miras@gmail.com>

--- test/openshift/e2e/ginkgo/sequential/1-110_validate_podsecurity_alerts_test.go
+++ test/openshift/e2e/ginkgo/sequential/1-110_validate_podsecurity_alerts_test.go
@@ -1,16 +1,12 @@
 package sequential
 
 import (
-	"context"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture"
 	k8sFixture "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/k8s"
-	fixtureUtils "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/utils"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("GitOps Operator Sequential E2E Tests", func() {
@@ -21,7 +17,7 @@ var _ = Describe("GitOps Operator Sequential E2E Tests", func() {
 			fixture.EnsureSequentialCleanSlate()
 		})
 
-		It("verifies openshift-gitops: operator sets podSecurityLabelSync and OpenShift populates pod-security labels", func() {
+		It("verifies openshift-gitops: operator sets podSecurityLabelSync and OpenShift sets audit to restricted", func() {
 			gitopsNS := &corev1.Namespace{
 				ObjectMeta: metav1.ObjectMeta{
 					Name: "openshift-gitops",
@@ -33,38 +29,9 @@ var _ = Describe("GitOps Operator Sequential E2E Tests", func() {
 			Eventually(gitopsNS, "5m", "5s").Should(
 				k8sFixture.HaveLabelWithValue("security.openshift.io/scc.podSecurityLabelSync", "true"))
 
-			By("OpenShift populates at least one pod-security.kubernetes.io/* label")
-			pssLabelKeys := []string{
-				"pod-security.kubernetes.io/audit",
-				"pod-security.kubernetes.io/audit-version",
-				"pod-security.kubernetes.io/enforce",
-				"pod-security.kubernetes.io/enforce-version",
-				"pod-security.kubernetes.io/warn",
-				"pod-security.kubernetes.io/warn-version",
-			}
-			Eventually(func() bool {
-				k8sClient, _ := fixtureUtils.GetE2ETestKubeClient()
-				ns := &corev1.Namespace{}
-				if err := k8sClient.Get(context.Background(), client.ObjectKey{Name: "openshift-gitops"}, ns); err != nil {
-					return false
-				}
-				if ns.Labels == nil {
-					return false
-				}
-				for _, key := range pssLabelKeys {
-					if ns.Labels[key] != "" {
-						return true
-					}
-				}
-				return false
-			}, "5m", "5s").Should(BeTrue(), "expected at least one pod-security.kubernetes.io/* label to be set by OpenShift")
-
-			k8sClient, _ := fixtureUtils.GetE2ETestKubeClient()
-			ns := &corev1.Namespace{}
-			Expect(k8sClient.Get(context.Background(), client.ObjectKey{Name: "openshift-gitops"}, ns)).To(Succeed())
-			for _, key := range pssLabelKeys {
-				GinkgoWriter.Printf("observed namespace label %s=%q\n", key, ns.Labels[key])
-			}
+			By("OpenShift sets pod-security.kubernetes.io/audit=restricted (pod-security *-version labels vary by cluster and are not asserted)")
+			Eventually(gitopsNS, "5m", "5s").Should(
+				k8sFixture.HaveLabelWithValue("pod-security.kubernetes.io/audit", "restricted"))
 		})
 
 	})
