ac88a012a866a38c13e15a5ed27646c650fd7248
Varsha B <vab@redhat.com>
2026-04-08 03:14:43 +0530

    Update gitops console and gitops backend images (#1100)
    * update gitosp console and gitops backend images
    
    Signed-off-by: Varsha B <vab@redhat.com>
    
    * fix images
    
    Signed-off-by: Varsha B <vab@redhat.com>
    
    ---------
    
    Signed-off-by: Varsha B <vab@redhat.com>

--- common/common.go
+++ common/common.go
@@ -30,9 +30,9 @@ const (
 	// InfraNodeLabelSelector is a nodeSelector for infrastructure nodes in Openshift
 	InfraNodeLabelSelector = "node-role.kubernetes.io/infra"
 	// Default console plugin image
-	DefaultConsoleImage = "quay.io/redhat-developer/gitops-console-plugin"
+	DefaultConsoleImage = "quay.io/redhat-user-workloads/rh-openshift-gitops-tenant/console-plugin-rhel9"
 	// Default console plugin version
-	DefaultConsoleVersion = "v0.1.0"
+	DefaultConsoleVersion = "main"
 	// Default console plugin installation OCP version
 	DefaultDynamicPluginStartOCPVersion = "4.15.0"
 	// ImagePullPolicyEnvVar is the environment variable for configuring image pull policy
