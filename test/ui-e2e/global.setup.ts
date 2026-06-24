8eaa716c1cbdb2b66fba1997feb02093705399f2
Triona Doyle <tekton@example.com>
2026-06-24 13:52:51 +0100

    test updates and address coderabbit feedback
    Signed-off-by: Triona Doyle <tekton@example.com>

--- test/ui-e2e/global.setup.ts
+++ test/ui-e2e/global.setup.ts
@@ -13,10 +13,10 @@ async function globalSetup() {
     execSync('oc delete all -l app=spring-petclinic -n openshift-gitops --wait=false', { stdio: 'ignore' });
 
     console.log('* Cluster sanitized. Starting test suite.');
-    } catch (error) {
+  } catch (error) {
     console.error('Pre-flight cleanup failed. Check your cluster connection.', error);
     throw error;
-    }
+  }
 }
 
 export default globalSetup;
