b6a4f8ecc06713bdd48815cb5f468069c0a5699e
Anand Kumar Singh <anandrkskd@gmail.com>
2026-09-02 18:07:03 +0530

    fix parallel tests
    Signed-off-by: Anand Kumar Singh <anandrkskd@gmail.com>

--- test/openshift/e2e/ginkgo/parallel/1-081_validate_applicationset_deployment_test.go
+++ test/openshift/e2e/ginkgo/parallel/1-081_validate_applicationset_deployment_test.go
@@ -17,34 +17,59 @@ limitations under the License.
 package parallel
 
 import (
+	"context"
+
+	argov1beta1api "github.com/argoproj-labs/argocd-operator/api/v1beta1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture"
 	argocdFixture "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/argocd"
 	deploymentFixture "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/deployment"
 	k8sFixture "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/k8s"
+	fixtureUtils "github.com/redhat-developer/gitops-operator/test/openshift/e2e/ginkgo/fixture/utils"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/uuid"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 
 	Context("1-081_validate_applicationset_deployment", func() {
 
+		var (
+			k8sClient client.Client
+			ctx       context.Context
+		)
+
 		BeforeEach(func() {
 			fixture.EnsureParallelCleanSlate()
+			k8sClient, _ = fixtureUtils.GetE2ETestKubeClient()
+			ctx = context.Background()
 		})
 
-		It("verifies that openshift-gitops Argo CD has applicationset controller workload and service with expected values", Label("openshift"), func() {
+		It("verifies that an Argo CD instance has applicationset controller workload and service with expected values", func() {
 
-			gitopsArgoCD, err := argocdFixture.GetOpenShiftGitOpsNSArgoCD()
-			Expect(err).ToNot(HaveOccurred())
-			Eventually(gitopsArgoCD, "5m", "5s").Should(argocdFixture.BeAvailable())
+			ns, cleanupFunc := fixture.CreateRandomE2ETestNamespaceWithCleanupFunc()
+			defer cleanupFunc()
 
-			appsetController := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "openshift-gitops-applicationset-controller", Namespace: gitopsArgoCD.Namespace}}
-			Expect(appsetController).To(k8sFixture.ExistByName())
+			argoCDName := "argocd-" + string(uuid.NewUUID())[0:8]
+			appsetName := argoCDName + "-applicationset-controller"
+
+			By("creating Argo CD CR with ApplicationSet controller enabled")
+			argoCD := &argov1beta1api.ArgoCD{
+				ObjectMeta: metav1.ObjectMeta{Name: argoCDName, Namespace: ns.Name},
+				Spec: argov1beta1api.ArgoCDSpec{
+					ApplicationSet: &argov1beta1api.ArgoCDApplicationSet{},
+				},
+			}
+			Expect(k8sClient.Create(ctx, argoCD)).To(Succeed())
+			Eventually(argoCD, "5m", "5s").Should(argocdFixture.BeAvailable())
+
+			appsetController := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: appsetName, Namespace: ns.Name}}
+			Eventually(appsetController, "5m", "5s").Should(k8sFixture.ExistByName())
 			Eventually(appsetController).Should(deploymentFixture.HaveReadyReplicas(1))
 
 			Expect(appsetController.Spec.Template.Spec.Containers[0].Ports).To(Equal([]corev1.ContainerPort{
@@ -53,9 +78,9 @@ var _ = Describe("GitOps Operator Parallel E2E Tests", func() {
 			}))
 
 			appsetService := &corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{Name: "openshift-gitops-applicationset-controller", Namespace: gitopsArgoCD.Namespace},
+				ObjectMeta: metav1.ObjectMeta{Name: appsetName, Namespace: ns.Name},
 			}
-			Expect(appsetService).To(k8sFixture.ExistByName())
+			Eventually(appsetService).Should(k8sFixture.ExistByName())
 			Expect(appsetService.Spec.Ports).To(Equal([]corev1.ServicePort{
 				{Port: 7000, Name: "webhook", Protocol: "TCP", TargetPort: intstr.FromInt(7000)},
 				{Port: 8080, Name: "metrics", Protocol: "TCP", TargetPort: intstr.FromInt(8080)},
