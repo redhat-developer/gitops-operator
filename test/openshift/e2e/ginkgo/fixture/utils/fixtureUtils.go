4a225a7f3f7aadd4fc34f32394cf7280061da9df
Atif Ali <atali@redhat.com>
2026-09-21 15:12:46 -0400

    add e2e and docs for operand priorityClassName
    Signed-off-by: Atif Ali <atali@redhat.com>

--- test/openshift/e2e/ginkgo/fixture/utils/fixtureUtils.go
+++ test/openshift/e2e/ginkgo/fixture/utils/fixtureUtils.go
@@ -32,6 +32,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	schedulingv1 "k8s.io/api/scheduling/v1"
 	crdv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 
@@ -149,6 +150,10 @@ func getKubeClient(config *rest.Config) (client.Client, *runtime.Scheme, error) 
 	}
 
 	if err := certificatesv1beta1.AddToScheme(scheme); err != nil {
+		return nil, nil, err
+	}
+
+	if err := schedulingv1.AddToScheme(scheme); err != nil {
 		return nil, nil, err
 	}
 
